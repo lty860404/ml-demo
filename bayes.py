077e2dff7c853929d9325f6892e57762a2297dd1
lty860404 <lty860404@gmail.com>
2018-10-08 18:32:03 +0800

    learning Ch04

--- bayes.py
+++ bayes.py
@@ -25,6 +25,15 @@ def setOfWords2Vec(vocabList, inputSet):
 			print("the word:%s is not in my vocabulary!"%word)
 	return returnVec
 
+def bagOfWords2Vec(vocabList,inputSet):
+	returnVec = [0]*len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)]+=1
+		#else:
+			#print("the word:%s is not in my vocabulary!"%word)
+	return returnVec
+
 def trainNB0(trainMatrix,trainCategory):
 	numTrainDocs = len(trainMatrix)
 	numWords = len(trainMatrix[0])
@@ -47,5 +56,105 @@ def trainNB0(trainMatrix,trainCategory):
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
 	p1 = sum(vec2Classify*p1Vec) + log(pClass1)
 	p0 = sum(vec2Classify*p0Vec) + log(1.0 - pClass1)
-	
+	if p1 > p0:
+		return 1
+	else:
+		return 0
+
+def testingNB(testEntry):
+	listOPosts,listClasses = loadDataSet()
+	myVocabList = createVocabList(listOPosts)
+	trainMat = []
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+	p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry,'classified as:',classifyNB(thisDoc,p0V,p1V,pAb)
+
+def textParse(bigString):
+	import re
+	listOfTokens = re.split(r'\W*', bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest(basePath):
+	docList = []; classList = []; fullText = []
+	for i in range(1,26):
+		spamPath = basePath + ('spam/%d.txt'%i)
+		wordList = textParse(open(spamPath).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		hamPath = basePath + ('ham/%d.txt'%i)
+		wordList = textParse(open(hamPath).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList = createVocabList(docList)
+	trainingSet = range(50); testSet = []
+	for i in range(10):
+		randIndex = int(random.uniform(0,len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat = []; trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+	errorCount = 0
+	for docIndex in testSet:
+		wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+			print 'classification error:',docList[docIndex]
+			errorCount += 1
+	print 'the error rate is:',float(errorCount)/len(testSet)
+
+def calcMostFreq(vocabList,fullText):
+	import operator
+	freqDict = {}
+	for token in vocabList:
+		freqDict[token] = fullText.count(token)
+	sortedFreq = sorted(freqDict.iteritems(), key = operator.itemgetter(1), reverse=True)
+	return sortedFreq[:30]
+
+#http://www.nasa.gov/rss/dyn/image_of_the_day.rss
+#https://sports.yahoo.com/nba/teams/hou/rss.xml
+
+def localWords(feed1,feed0):
+	import feedparser
+	docList = [];classList = [];fullText = []
+	minLen = min(len(feed1['entries']),len(feed0['entries']))
+	for i in range(minLen):
+		wordList = textParse(feed1['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList = textParse(feed0['entries'][i]['summary'])
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList = createVocabList(docList)
+	top30Words = calcMostFreq(vocabList, fullText)
+	for pairW in top30Words:
+		if pairW[0] in vocabList: vocabList.remove(pairW[0])
+	trainingSet = range(2*minLen); testSet = []
+	for i in range(20):
+		randIndex = int(random.uniform(0, len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat = []; trainClasses = []
+	for docIndex in trainingSet:
+		trainMat.append(bagOfWords2Vec(vocabList, docList[docIndex]))
+		trainClasses.append(classList[docIndex])
+	p0V,p1V,pClass1 = trainNB0(array(trainMat),array(trainClasses))
+	errorCount = 0
+	for docIndex in testSet:
+		wordVector = bagOfWords2Vec(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector), p0V, p1V, pClass1) != classList[docIndex]:
+			errorCount += 1
+			print 'classification error',docList[docIndex]
+	print 'the error rate:',float(errorCount)/len(testSet)
+	return vocabList,p0V,p1V
+
+
+
 
