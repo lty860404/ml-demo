57a95483330389de030ed63a80f99aa22823a26e
lty860404 <lty860404@gmail.com>
2019-01-02 22:45:26 +0800

    add platt smo method

--- svmMLiA.py
+++ svmMLiA.py
@@ -1,7 +1,7 @@
 from numpy import *
 
 #/home/lty/machine-learn/scripts/python-workspace/machinelearninginaction/Ch06/testSet.txt
-
+#D:\\MachineLearnWorkSpace\\machinelearninginaction\\Ch06\\testSet.txt
 
 def loadDataSet(fileName):
     dataMat = []
@@ -13,13 +13,11 @@ def loadDataSet(fileName):
         labelMat.append(float(lineArr[2]))
     return dataMat, labelMat
 
-
 def selectJrand(i, m):
     j = i
     while (j == i):
         j = int(random.uniform(0, m))
     return j
-
 
 def clipAlpha(aj, H, L):
     if aj > H:
@@ -27,7 +25,6 @@ def clipAlpha(aj, H, L):
     if L > aj:
         aj = L
     return aj
-
 
 def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     dataMatrix = mat(dataMatIn)
@@ -108,6 +105,78 @@ def selectJ(i, oS, Ei):
             deltaE = abs(Ei - Ek)
             if (deltaE > maxDeltaE):
                 maxK = k; maxDeltaE = deltaE; Ej = Ek
+        return maxK, Ej
+    else:
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+        return j,Ej
+
+def updateEk(oS, k):
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1,Ek]
+
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or\
+        ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j,Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy();
+        if (oS.labelMat[i] != oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L==H: print("L==H"); return 0
+        eta = 2.0*oS.X[i,:]*oS.X[j,:].T - oS.X[i,:]*oS.X[i,:].T - oS.X[j,:]*oS.X[j,:].T
+        if eta >=0: print("eta>=0"); return 0
+        oS.alphas[j] -= oS.labelMat[j]*(Ei-Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j],H,L)
+        updateEk(oS,j)
+        if(abs(oS.alphas[j]-alphaJold) < 0.00001): print("j not moving enough"); return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold-oS.alphas[j])
+        updateEk(oS,i)
+        b1 = oS.b - Ei - oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[i,:].T -\
+             oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
+        b2 = oS.b - Ej - oS.labelMat[i]*(oS.alphas[i]-alphaIold)*oS.X[i,:]*oS.X[j,:].T -\
+             oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
+        if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
+        if (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
+        else: oS.b = (b1+b2)/2.0
+        return 1
+    else:
+        return 0
+
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+    oS = optStruct(mat(dataMatIn), mat(classLabels).transpose(),C,toler)
+    iter = 0
+    entireSet = True; alphaPairsChanged = 0
+    while (iter<maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+        alphaPairsChanged = 0
+        if entireSet:
+            for i in range(oS.m):
+                alphaPairsChanged += innerL(i,oS)
+                print ("fullSet, iter:%d i:%d, pairs changed %d"%(iter,i,alphaPairsChanged))
+            iter += 1
+        else:
+            nonBoundIs = nonzero((oS.alphas.A > 0)*(oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i,oS)
+                print ("non-bound, iter:%d i:%d, pairs changed %d"%(iter,i,alphaPairsChanged))
+            iter += 1
+        if entireSet: entireSet = False
+        elif (alphaPairsChanged == 0): entireSet = True
+        print("iteration number: %d"%iter)
+    return oS.b, oS.alphas
+
+def calcWs(alphas, dataArr, classLabels):
+    X = mat(dataArr); labelMat = mat(classLabels).transpose()
+    m,n = shape(X)
+    w = zeros((n,1))
+    for i in range(m):
+        w += multiply(alphas[i]*labelMat[i],X[i,:].T)
+    return w
+
 
 
 
